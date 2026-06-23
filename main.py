8d968abff62512ab78d1ca386bccadb37335c7e1
Lirkan800 <Lirancohen182@gmail.com>
2026-06-23 22:10:25 +0300

    Change bonus streak to 2 exact hits

--- main.py
+++ main.py
@@ -473,8 +473,8 @@ def finish_match_and_calculate(match_to_finish, actual_home, actual_away):
             if is_exact:
                 players[player_name]["streak"] += 1
 
-                if players[player_name]["streak"] == 3:
-                    bonus = 6
+                if players[player_name]["streak"] == 2:
+                    bonus = 4
                     players[player_name]["streak"] = 0
             else:
                 players[player_name]["streak"] = 0
@@ -1355,8 +1355,8 @@ def admin_test():
                 points = exact_points
                 players[selected_player]["streak"] += 1
 
-                if players[selected_player]["streak"] == 3:
-                    bonus = 6
+                if players[selected_player]["streak"] == 2:
+                    bonus = 4
                     players[selected_player]["streak"] = 0
                     points_text = f"פגיעה מדויקת! +{points} וגם בונוס +6"
                 else:
